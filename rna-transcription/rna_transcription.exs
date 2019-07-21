c1d5ae3feb37b8b779b42f90d57bcc9ac0d1e1c0
bghalami <benjaminghalami@gmail.com>
2019-07-21 15:38:02 -0600

    All tests passing

--- rna-transcription/rna_transcription.exs
+++ rna-transcription/rna_transcription.exs
@@ -7,7 +7,26 @@ defmodule RNATranscription do
   iex> RNATranscription.to_rna('ACTG')
   'UGAC'
   """
+
   @spec to_rna([char]) :: [char]
+  def to_rna(dna) when length(dna) == 1 do
+    map = %{
+      'G' => 'C',
+      'C' => 'G',
+      'T' => 'A',
+      'A' => 'U'
+    }
+    map[dna]
+  end
+
   def to_rna(dna) do
+    map = %{
+      "G" => 67,
+      "C" => 71,
+      "T" => 65,
+      "A" => 85
+    }
+    dna
+    |> Enum.map(fn el -> map[<<el>>] end)
   end
 end
