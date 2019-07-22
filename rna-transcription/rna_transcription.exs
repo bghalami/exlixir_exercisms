86a65ae04ccab7e57c8b782cc615efee0eef0816
bghalami <benjaminghalami@gmail.com>
2019-07-22 12:24:17 -0600

    Finished optimizing

--- rna-transcription/rna_transcription.exs
+++ rna-transcription/rna_transcription.exs
@@ -9,16 +9,6 @@ defmodule RNATranscription do
   """
 
   @spec to_rna([char]) :: [char]
-  def to_rna(dna) when length(dna) == 1 do
-    map = %{
-      'G' => 'C',
-      'C' => 'G',
-      'T' => 'A',
-      'A' => 'U'
-    }
-    map[dna]
-  end
-
   def to_rna(dna) do
     map = %{
       ?G => ?C,
@@ -26,6 +16,7 @@ defmodule RNATranscription do
       ?T => ?A,
       ?A => ?U
     }
+
     dna
     |> Enum.map(&(map[&1]))
   end
