8a87aaed29f065908d5810b4d77b0177566aea54
bghalami <benjaminghalami@gmail.com>
2019-07-24 12:45:05 -0600

    Refactored word count solution

--- acronym/acronym.exs
+++ acronym/acronym.exs
@@ -5,5 +5,16 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    Regex.split(~r/[\s-]/, string)
+    |> Enum.map(fn word -> custom_cap(word, multiple_caps?(word)) end)
+    # |> Enum.join()
+    # |> String.split("")
+    # |> Enum.filter(fn letter -> Regex.match?(~r/[A-Z]/, letter) end)
+    # |> Enum.join()
   end
+
+  defp custom_cap(word, true), do: word
+  defp custom_cap(word, _), do: String.capitalize(word)
+
+  defp multiple_caps?(word), do: String.match?(word, ~r/[A-Z{2,}]/)
 end
--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -1,4 +1,7 @@
 defmodule Words do
+
+  @puncuation_getter ~r/[\s_!@#$%^&*(),:;]/
+
   @doc """
   Count the number of words in the sentence.
 
@@ -8,14 +11,10 @@ defmodule Words do
   def count(sentence) do
     sentence
     |> String.downcase()
-    |> String.replace(puncuation_getter(), " ")
-    |> String.split()
+    |> String.split(@puncuation_getter, trim: true)
     |> Enum.group_by(fn word -> word end)
     |> Enum.map(fn {k,v} -> {k, length(v)} end)
     |> Map.new
   end
 
-  def puncuation_getter do
-    ~r/[_!@#$%^&*(),:;]/
-  end
 end
