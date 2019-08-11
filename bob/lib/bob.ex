9e9c33634238f8fb2442876783196cf725f740e4
bghalami <benjaminghalami@gmail.com>
2019-08-11 13:59:09 -0600

    Replaced regex with conditional

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -15,19 +15,19 @@ defmodule Bob do
   end
 
   @spec yell_question(String.t()) :: boolean
-  def yell_question(input) do
-    String.last(input) == "?" && String.upcase(input) == input && String.match?(input, ~r/[A-Z]/)
+  defp yell_question(input) do
+    String.last(input) == "?" && String.upcase(input) == input && String.upcase(input) != String.downcase(input)
   end
   @spec ask_question(String.t()) :: boolean
-  def ask_question(input) do
+  defp ask_question(input) do
     String.last(input) == "?"
   end
   @spec silent_treatment(String.t()) :: boolean
-  def silent_treatment(input) do
+  defp silent_treatment(input) do
     String.trim(input) == ""
   end
   @spec yelling(String.t()) :: boolean
-  def yelling(input) do
-    String.upcase(input) == input && String.match?(input, ~r/\p{L}/u)
+  defp yelling(input) do
+    String.upcase(input) == input && String.upcase(input) != String.downcase(input)
   end
 end
