4dd45a22d8a95d40592c3cfbfb5f0793b0d9daa2
bghalami <benjaminghalami@gmail.com>
2019-08-12 10:06:11 -0600

    Changed function names to meet convention

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -1,33 +1,33 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      yell_question(input) ->
+      yell_question?(input) ->
         "Calm down, I know what I'm doing!"
-      ask_question(input) ->
+      ask_question?(input) ->
         "Sure."
-      silent_treatment(input) ->
+      silent_treatment?(input) ->
         "Fine. Be that way!"
-      yelling(input) ->
+      yelling?(input) ->
         "Whoa, chill out!"
       true ->
         "Whatever."
     end
   end
 
-  @spec yell_question(String.t()) :: boolean
-  defp yell_question(input) do
+  @spec yell_question?(String.t()) :: boolean
+  defp yell_question?(input) do
     String.last(input) == "?" && String.upcase(input) == input && String.upcase(input) != String.downcase(input)
   end
-  @spec ask_question(String.t()) :: boolean
-  defp ask_question(input) do
+  @spec ask_question?(String.t()) :: boolean
+  defp ask_question?(input) do
     String.last(input) == "?"
   end
-  @spec silent_treatment(String.t()) :: boolean
-  defp silent_treatment(input) do
+  @spec silent_treatment?(String.t()) :: boolean
+  defp silent_treatment?(input) do
     String.trim(input) == ""
   end
-  @spec yelling(String.t()) :: boolean
-  defp yelling(input) do
+  @spec yelling?(String.t()) :: boolean
+  defp yelling?(input) do
     String.upcase(input) == input && String.upcase(input) != String.downcase(input)
   end
 end
