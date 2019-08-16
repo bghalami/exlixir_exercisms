8faaa8f86f06fcc18bc810a58d024262d3dac82b
bghalami <benjaminghalami@gmail.com>
2019-08-16 10:56:09 -0600

    Made mentor changes

--- beer-song/lib/beer_song.ex
+++ beer-song/lib/beer_song.ex
@@ -3,18 +3,30 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
-  def verse(number), do: "#{line_one(number)}\n#{line_two(number)}\n"
-
-  @spec line_one(integer) :: String.t()
-  defp line_one(1), do: "1 bottle of beer on the wall, 1 bottle of beer."
-  defp line_one(0), do: "No more bottles of beer on the wall, no more bottles of beer."
-  defp line_one(number), do: "#{number} bottles of beer on the wall, #{number} bottles of beer."
-
-  @spec line_two(integer) :: String.t()
-  defp line_two(2), do: "Take one down and pass it around, 1 bottle of beer on the wall."
-  defp line_two(1), do: "Take it down and pass it around, no more bottles of beer on the wall."
-  defp line_two(0), do: "Go to the store and buy some more, 99 bottles of beer on the wall."
-  defp line_two(number), do: "Take one down and pass it around, #{number - 1} bottles of beer on the wall."
+  def verse(2) do
+    """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+  end
+  def verse(1) do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+  def verse(0) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+  def verse(number) do
+    """
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+    """
+  end
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
